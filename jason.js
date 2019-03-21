44e9382bbaabf26543a8527313e4f2819dc5ed97
jasonsupertrampp <44702002+jasonsupertrampp@users.noreply.github.com>
2019-03-21 20:04:08 +0300

    Update jason.js

--- jason.js
+++ jason.js
@@ -3,6 +3,8 @@ function openNav() {
 if ($(window).width() < 960) {
   document.getElementById("mySidenav").style.width = "50%";
   document.getElementById("mySidenav").style.display="block";
+  document.querySelector(".logo").style.visibility="hidden";
+  document.querySelector(".navbar-toggler").style.visibility="hidden";
 }
 else {
   document.getElementById("mySidenav").style.width = "15%";
@@ -16,4 +18,6 @@ function closeNav() {
 document.getElementById("mySidenav").style.width = "0";
 document.querySelector(".main").style.marginLeft= "0";
 document.body.style.backgroundColor = "white";
+  document.querySelector(".logo").style.visibility="inherit";
+  document.querySelector(".navbar-toggler").style.visibility="inherit";
 }
