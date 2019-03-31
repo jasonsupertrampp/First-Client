3095a3fcabd9eb5202bb98f6a28c72289d702127
Yasin <44702002+jasonsupertrampp@users.noreply.github.com>
2019-04-01 00:02:37 +0300

    animation part 1

---
+++ jsn.js
@@ -0,0 +1,11 @@
+document.querySelector('.daha-fazla').addEventListener("click",function(){
+
+document.querySelector('.diger-animasyon').style.display="block";
+AOS.init();
+hide();
+
+
+});
+function hide(){
+  document.querySelector('.daha-fazla').style.display="none";  AOS.init();
+}
