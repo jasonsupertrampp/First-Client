05154c4f00562adf81d201c420aeb44e232d369a
Yasin <44702002+jasonsupertrampp@users.noreply.github.com>
2019-04-12 21:08:57 +0300

    about us part

---
+++ js/hak.js
@@ -0,0 +1,22 @@
+function change(){
+  document.getElementById('main-img').src='https://images.pexels.com/photos/1663263/pexels-photo-1663263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+  document.getElementById('main-href').href='https://images.pexels.com/photos/1663263/pexels-photo-1663263.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+}
+function change1(){
+  document.getElementById('main-img').src='https://images.pexels.com/photos/1358900/pexels-photo-1358900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+  document.getElementById('main-href').href='https://images.pexels.com/photos/1358900/pexels-photo-1358900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+}
+function change2(){
+  document.getElementById('main-img').src='https://images.pexels.com/photos/1080721/pexels-photo-1080721.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+  document.getElementById('main-href').href='https://images.pexels.com/photos/1080721/pexels-photo-1080721.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+}
+function change3(){
+  document.getElementById('main-img').src='https://images.pexels.com/photos/273822/pexels-photo-273822.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+  document.getElementById('main-href').href='https://images.pexels.com/photos/273822/pexels-photo-273822.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+
+}
+function change4(){
+  document.getElementById('main-img').src='https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+  document.getElementById('main-href').href='https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940';
+
+}
---
+++ js/jsn.js
@@ -0,0 +1,73 @@
+document.querySelector('.daha-fazla').addEventListener("click",function(){
+
+document.querySelector('.diger-animasyon').style.display="block";
+AOS.init();
+hide();
+
+
+});
+function hide(){
+  document.querySelector('.daha-fazla').style.display="none";  AOS.init();
+}
+
+
+
+
+
+function openNav() {
+// document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+if ($(window).width() < 960) {
+  document.getElementById("mySidenav").style.width = "50%";
+  document.getElementById("mySidenav").style.display="block";
+  document.querySelector(".logo").style.visibility="hidden";
+  document.querySelector(".navbar-toggler").style.visibility="hidden";
+}
+else {
+  document.getElementById("mySidenav").style.width = "15%";
+  document.querySelector(".main").style.marginLeft = "15%";
+  document.getElementById("mySidenav").style.display="block";
+}
+
+}
+
+function toggleClose(){
+  var x=document.querySelector('.burger');
+  if(x.style.display ==="none"){
+    x.style.display="block";
+  }else{
+    x.style.display="none";
+  }
+
+}
+
+
+// function anotherClose(){
+//   var x=document.querySelector('#navbarNav');
+//   if(x.style.display ==="none"){
+//     x.style.display="block";
+//   }else{
+//     x.style.display="none";
+//   }
+// }
+
+document.querySelector(".trend").addEventListener("touchstart",function(){
+closeNav();
+// anotherClose();
+});
+document.querySelector(".page-footer").addEventListener("touchstart",function(){
+closeNav();
+});
+document.querySelector(".other-products").addEventListener("touchstart",function(){
+closeNav();
+});
+document.querySelector("#title").addEventListener("touchstart",function(){
+closeNav();
+});
+
+function closeNav() {
+document.getElementById("mySidenav").style.width = "0";
+document.querySelector(".main").style.marginLeft= "0";
+document.body.style.backgroundColor = "white";
+document.querySelector(".logo").style.visibility="inherit";
+document.querySelector(".navbar-toggler").style.visibility="inherit";
+}
